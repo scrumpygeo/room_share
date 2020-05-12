335406379dd0fe551aa299e38f420018a1112687
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-05-12 20:17:43 +0100

    update devise edit and net

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :rooms, dependent: :destroy
   has_many :bookings, through: :rooms, dependent: :destroy
+
+  has_one_attached :photo
 end
