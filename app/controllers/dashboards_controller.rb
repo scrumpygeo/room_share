82dd22d8e5b814569a4cdcc68138abe16cdbe46d
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-05-12 11:52:50 +0100

    hosts show page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,21 @@
 class DashboardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room, only: [:show]
+
   def index 
     @rooms = Room.where(user: current_user).order('name ASC')
+  end
+
+  def show 
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description, :price, :street, :city, :guest_nr, :photo)
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create]
   end
   resources :bookings, only: [:index, :show, :destroy, :update] 
-  resources :dashboards, only: [:index]
+  resources :dashboards, only: [:index, :show]
 end
