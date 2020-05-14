1fa4e536d2df65fc5c216a37330f7cb7898be4b3
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-05-14 16:49:10 +0100

    tidy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,16 @@ Booking.destroy_all
 Room.destroy_all
 User.destroy_all
 
-puts 'Creating 5 users...'
-5.times do
-  user = User.new(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: "123456"
-  )
-  user.save!
-end
-
-puts 'Creating Fred'
-user = User.new(
-  first_name: 'Fred',
-  last_name: 'Bloggs',
-  email: 'fred@bloggs.me',
-  password: '123456'
-)
-user.save!
-
-puts 'Creating Wilma'
-user = User.new(
-  first_name: 'Wilma',
-  last_name: 'Flintstone',
-  email: 'wilma@flintstone.me',
-  password: '123456'
-)
-user.save!
+# puts 'Creating 5 users...'
+# 5.times do
+#   user = User.new(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "abcdefgh12345678"
+#   )
+#   user.save!
+# end
 
 # use csv for rooms
 
@@ -58,22 +40,6 @@ csv.each do |row|
 end
 
 puts "There are now #{Room.count} rows in the rooms table"
-
-# puts 'Creating 10 rooms...'
-# 10.times do
-#   room = Room.new(
-#     name:    Faker::Address.community,
-#     description: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
-#     street: Faker::Address.street_address, 
-#     city:  %w[ London Paris Rome Cricklewood ].sample,
-#     # city: Faker::Address.city,
-#     price:  rand(25..50),
-#     guest_nr: rand(1..4),
-#     user: User.all.sample
-#   )
-#   room.photo = "https://source.unsplash.com/400x300/?bedrooms"
-#   room.save!
-# end
 
 puts 'Creating 10 bookings...'
 10.times do
